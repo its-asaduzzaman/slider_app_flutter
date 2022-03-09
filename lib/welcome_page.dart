3b5394e6c9ce162ac4eaacc643a8cd2af65d5726
AsaduzzamanSam <samrat.asad@gmail.com>
2022-03-09 18:49:29 +0600

    add vertical slider and image

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,8 +10,30 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  List images =['welcome-one.png',
+    'welcome-two.png',
+        'welcome-three.png'];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index){
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "img/"+images[index]
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+
+      }),
+    );
   }
 }
